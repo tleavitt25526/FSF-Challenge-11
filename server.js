5d83e464e7113ca0a9c30bcf1aa7fb32305876d4
tleavitt25526 <tleavitt25526@gmail.com>
2024-10-13 10:24:16 -0400

    Finished routing, moving on to file writing

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
-
+const fs = require('node:fs')
+const {v4, uuidv4 } = require('uuid');  
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 // Import custom middleware, "cLog"
-app.use(clog);
+//app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+//app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -26,6 +25,33 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    }
+    console.log(data);
+    res.send(data);
+  });
+})
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    
+  }
+})
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
